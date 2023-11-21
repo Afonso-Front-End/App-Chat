9275a51d7a2b73fba6a80e2073a22145ea7adfa2
Afonso <afonsogithub@gmail.com>
2023-11-21 13:50:51 -0300

    Atualizando

--- src/components/select-users/Select.js
+++ src/components/select-users/Select.js
@@ -1,76 +1,9 @@
-import React, { useEffect, useState } from "react";
-import { jwtDecode } from "jwt-decode";
 
 import imgGitAnonimus from '../../assets/img/img-git-Anonimo.jpg'
 import Navigation from '../navigation/Navigation'
 
 export default function Select() {
-  const [userInfo, setUserInfo] = useState(null);
-
-  useEffect(() => {
-    // Função para decodificar o token
-    const decodeToken = (token) => {
-      try {
-        if (typeof token === "string") {
-          const decoded = jwtDecode(token);
-          return decoded;
-        } else {
-          console.error("O token não é uma string válida:", token);
-          return null;
-        }
-      } catch (error) {
-        console.error("Erro ao decodificar o token:", error);
-        return null;
-      }
-    };
-
-    // Ouvinte para a mensagem enviada pela página de login
-    const messageListener = (event) => {
-      // Verifique se a origem da mensagem é a esperada
-      if (event.origin === 'https://login-users-systen.netlify.app') {
-        // Verifique se a mensagem contém um token válido
-        if (event.data && event.data.token) {
-          const token = event.data.token;
-
-          // Faça algo com o token
-          console.log('Token recebido:', token);
-
-          // Decodificando o token para obter as informações do usuário
-          const decodedUserInfo = decodeToken(token);
-
-          // Atualizando o estado com as informações do usuário
-          setUserInfo(decodedUserInfo);
-        } else {
-          console.error('Mensagem inválida - Token ausente');
-        }
-      }
-    };
-
-    // Adiciona o ouvinte de mensagem
-    window.addEventListener('message', messageListener);
-
-    // Obtendo o token do localStorage do outro domínio
-    const otherDomainToken = window.localStorage.getItem('token');
-
-    // Se houver um token do outro domínio
-    if (otherDomainToken) {
-      // Decodificando o token para obter as informações do usuário
-      const decodedUserInfo = decodeToken(otherDomainToken);
-
-      // Atualizando o estado com as informações do usuário
-      setUserInfo(decodedUserInfo);
-    } else {
-      console.error('Token do outro domínio não encontrado.');
-    }
-
-    // Remove o ouvinte de mensagem quando o componente é desmontado
-    return () => {
-      window.removeEventListener('message', messageListener);
-    };
-  }, []);
-
-  console.log(userInfo);
-
+  
   return (
     <div className="area-select-user">
       <ul className="items-users">
