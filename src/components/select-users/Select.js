e25a54d60f7548970025a18d0aa51d9b905db7e8
Afonso <afonsogithub@gmail.com>
2023-11-21 13:19:00 -0300

    Atualizando

--- src/components/select-users/Select.js
+++ src/components/select-users/Select.js
@@ -9,54 +9,80 @@ export default function Select() {
   const [userInfo, setUserInfo] = useState(null);
 
   useEffect(() => {
-      // Função para decodificar o token
-      const decodeToken = (token) => {
-          try {
-              if (typeof token === "string") {
-                  const decoded = jwtDecode(token);
-                  return decoded;
-              } else {
-                  console.error("O token não é uma string válida:", token);
-                  return null;
-              }
-          } catch (error) {
-              console.error("Erro ao decodificar o token:", error);
-              return null;
-          }
-      };
-
-      // Obtendo o token do localStorage do outro domínio
-      const otherDomainToken = window.localStorage.getItem('token');
-
-      // Se houver um token do outro domínio
-      if (otherDomainToken) {
-          // Decodificando o token para obter as informações do usuário
-          const decodedUserInfo = decodeToken(otherDomainToken);
-
-          // Atualizando o estado com as informações do usuário
-          setUserInfo(decodedUserInfo);
-      } else {
-          console.error('Token do outro domínio não encontrado.');
+    // Função para decodificar o token
+    const decodeToken = (token) => {
+      try {
+        if (typeof token === "string") {
+          const decoded = jwtDecode(token);
+          return decoded;
+        } else {
+          console.error("O token não é uma string válida:", token);
+          return null;
+        }
+      } catch (error) {
+        console.error("Erro ao decodificar o token:", error);
+        return null;
       }
+    };
+
+    // Obtendo o token do localStorage do outro domínio
+    const otherDomainToken = window.localStorage.getItem('token');
+
+    // Se houver um token do outro domínio
+    if (otherDomainToken) {
+      // Decodificando o token para obter as informações do usuário
+      const decodedUserInfo = decodeToken(otherDomainToken);
+
+      // Atualizando o estado com as informações do usuário
+      setUserInfo(decodedUserInfo);
+    } else {
+      console.error('Token do outro domínio não encontrado.');
+    }
+
+    // Ouvinte para a mensagem enviada pela página de login
+    const messageListener = (event) => {
+      // Verifique se a origem da mensagem é a esperada
+      if (event.origin === 'https://login-users-systen.netlify.app') {
+        // Verifique se a mensagem contém um token válido
+        if (event.data && event.data.token) {
+          const token = event.data.token;
+  
+          // Faça algo com o token
+          console.log('Token recebido:', token);
+        } else {
+          console.error('Mensagem inválida - Token ausente');
+        }
+      }
+    };
+
+    // Adiciona o ouvinte de mensagem
+    window.addEventListener('message', messageListener);
+
+    // Remove o ouvinte de mensagem quando o componente é desmontado
+    return () => {
+      window.removeEventListener('message', messageListener);
+    };
   }, []);
 
-    console.log(userInfo)
-    return (
-        <div className="area-select-user">
-            <ul className="items-users">
-                <li className="user">
-                    <div className="img-user">
-                        <img src={imgGitAnonimus} alt="img-user" />
-                    </div>
+  console.log(userInfo);
 
-                    <div className="info-user">
-                        <div className="name-user"><span>Name</span></div>
-                        <div className="mensage-user"><p>Mensagem</p></div>
-                    </div>
-                </li>
-            </ul>
-            <Navigation />
+  console.log(userInfo)
+  return (
+    <div className="area-select-user">
+      <ul className="items-users">
+        <li className="user">
+          <div className="img-user">
+            <img src={imgGitAnonimus} alt="img-user" />
+          </div>
 
-        </div>
-    )
+          <div className="info-user">
+            <div className="name-user"><span>Name</span></div>
+            <div className="mensage-user"><p>Mensagem</p></div>
+          </div>
+        </li>
+      </ul>
+      <Navigation />
+
+    </div>
+  )
 }
