37e8dfb562f3320229d5a5b3ee161ab12493da23
Afonso <afonsogithub@gmail.com>
2023-11-22 16:33:59 -0300

    Atualizando

--- src/components/navigation/script.js
+++ src/components/navigation/script.js
@@ -6,32 +6,31 @@ export default function useScript() {
     const [resultadoPesquisa, setResultadoPesquisa] = useState(null);
 
     const handlePesquisarAmigo = async () => {
-        const inputPesquisa = document.getElementById('pesquisar-amigo');
-        const identifier = inputPesquisa.value;
-
-        try {
-            const resposta = await fetch(`https://api-planetscale-fawn.vercel.app/buscar-amigo/${identifier}`);
-            const dados = await resposta.json();
-
-            if (resposta.ok) {
-                setResultadoPesquisa(dados.amigo);
-            } else {
-                setResultadoPesquisa(null);
-                console.error('Erro ao pesquisar amigo:', dados.erro);
-            }
-        } catch (erro) {
-            console.error('Erro ao pesquisar amigo:', erro);
-
-            if (erro instanceof TypeError && erro.message === 'Failed to fetch') {
-                // Erro de rede
-                console.error('Erro de rede ao pesquisar amigo:', erro);
-            } else {
-                // Outro tipo de erro
-                console.error('Erro desconhecido ao pesquisar amigo:', erro);
-            }
-        }
-
-    }
+      const inputPesquisa = document.getElementById('pesquisar-amigo');
+      const identifier = inputPesquisa.value;
+  
+      try {
+          const resposta = await fetch(`https://api-planetscale-fawn.vercel.app/buscar-amigo/${identifier}`);
+          const dados = await resposta.json();
+  
+          if (resposta.ok) {
+              setResultadoPesquisa(dados.amigo);  // Ajuste aqui para dados.amigo.amigo
+          } else {
+              setResultadoPesquisa(null);
+              console.error('Erro ao pesquisar amigo:', dados.erro);
+          }
+      } catch (erro) {
+          console.error('Erro ao pesquisar amigo:', erro);
+  
+          if (erro instanceof TypeError && erro.message === 'Failed to fetch') {
+              // Erro de rede
+              console.error('Erro de rede ao pesquisar amigo:', erro);
+          } else {
+              // Outro tipo de erro
+              console.error('Erro desconhecido ao pesquisar amigo:', erro);
+          }
+      }
+  }
 
     const handleAdicionarAmigo = async () => {
         // Certifique-se de que há um resultado de pesquisa antes de tentar adicionar
@@ -57,7 +56,7 @@ export default function useScript() {
             console.error('Erro de rede ao adicionar amigo:', erro);
           }
         }
-      };
+    };
 
 
     const handleSearch = () => {
