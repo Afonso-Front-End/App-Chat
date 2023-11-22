a60a4223176d0793f9146bd4e9555bd6ccdd10be
Afonso <afonsogithub@gmail.com>
2023-11-22 12:19:19 -0300

    Atualizando

--- src/components/select-users/Select.js
+++ src/components/select-users/Select.js
@@ -1,9 +1,25 @@
+import React, { useState, useEffect } from 'react';
 
 import imgGitAnonimus from '../../assets/img/img-git-Anonimo.jpg'
 import Navigation from '../navigation/Navigation'
 
 export default function Select() {
-  
+  const [token, setToken] = useState('');
+
+  useEffect(() => {
+    // Recupera o token da URL
+    const urlParams = new URLSearchParams(window.location.search);
+    const tokenFromURL = urlParams.get('token');
+
+    // Armazena o token no localStorage
+    if (tokenFromURL) {
+      localStorage.setItem('token', tokenFromURL);
+      setToken(tokenFromURL);
+    }
+  }, []); 
+
+  console.log(token)
+
   return (
     <div className="area-select-user">
       <ul className="items-users">
