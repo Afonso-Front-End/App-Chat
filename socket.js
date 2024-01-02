efd80d1baeafe6ddfec5ba5146c0bd57ee747886
Afonso <129227018+Afonso-Front-End@users.noreply.github.com>
2024-01-02 13:23:43 -0300

    Atualziando

--- socket.js
+++ socket.js
@@ -2,5 +2,5 @@ import { io } from 'socket.io-client';
 
 // "undefined" means the URL will be computed from the `window.location` object
 const URL = process.env.NODE_ENV === 'production' ? undefined : 'https://api-planetscale-psi.vercel.app';
-
+console.log(URL)
 export const socket = io(URL);
