e40bddfd3b9bb5bd2d6a100d30755b60846c91e1
Afonso <129227018+Afonso-Front-End@users.noreply.github.com>
2024-01-07 13:01:28 -0300

    Atualizando

--- socket.js
+++ socket.js
@@ -1,6 +1,6 @@
+// No arquivo do cliente
 import { io } from 'socket.io-client';
 
-const URL = process.env.NODE_ENV === 'production' ? undefined : 'https://api-socket-io-chat-je6x0n7sk-afonsos-projects.vercel.app';
+const URL = process.env.NODE_ENV === 'production' ? 'https://api-socket-io-chat-web.vercel.app' : 'http://localhost:3001';
 
 export const socket = io(URL);
-
