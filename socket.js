b1cf97631f5fc4f1a4fcc3f40cfd39f12ab24fb5
Afonso <129227018+Afonso-Front-End@users.noreply.github.com>
2024-01-07 12:18:45 -0300

    Atualizando link

--- socket.js
+++ socket.js
@@ -1,6 +1,6 @@
 import { io } from 'socket.io-client';
 
-const URL = process.env.NODE_ENV === 'production' ? undefined : 'http://localhost:3001';
+const URL = process.env.NODE_ENV === 'production' ? undefined : 'https://api-socket-io-chat-je6x0n7sk-afonsos-projects.vercel.app';
 
 export const socket = io(URL);
 
