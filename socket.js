7cadbd95431fe92b16abae54833efc032bc9a6ab
Afonso-Front-End <129227018+Afonso-Front-End@users.noreply.github.com>
2024-02-03 11:38:24 -0300

    Atualizando chat no AR

--- socket.js
+++ socket.js
@@ -1,6 +1,5 @@
-// No arquivo do cliente
 import { io } from 'socket.io-client';
 
-const URL = process.env.NODE_ENV === 'production' ? 'https://api-socket-io-chat-web.vercel.app' : 'http://localhost:3001';
+const URL = process.env.NODE_ENV === 'production' ? '' : 'https://api-socketio-chat-web-production.up.railway.app/';
 
 export const socket = io(URL);
