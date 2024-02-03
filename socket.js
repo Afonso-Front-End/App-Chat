14c60df4b6e224249f26d7161759f60c8d888348
Afonso-Front-End <129227018+Afonso-Front-End@users.noreply.github.com>
2024-02-03 11:43:37 -0300

    Atualizando

--- socket.js
+++ socket.js
@@ -1,5 +1,5 @@
 import { io } from 'socket.io-client';
 
-const URL = process.env.NODE_ENV === 'production' ? '' : 'https://api-socketio-chat-web-production.up.railway.app/';
+const URL = process.env.NODE_ENV === 'production' ? '' : 'https://api-socketio-chat-web-production.up.railway.app';
 
 export const socket = io(URL);
