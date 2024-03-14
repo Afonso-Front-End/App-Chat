8b42324230cc7df414604472b730103dc78d82b2
Afonso-Front-End <129227018+Afonso-Front-End@users.noreply.github.com>
2024-03-14 12:15:08 -0300

    Teste

--- socket.js
+++ socket.js
@@ -1,7 +1,7 @@
 import { io } from 'socket.io-client';
 
-const productionURL = 'https://api-socketio-chat-web-production.up.railway.app';
-// const developmentURL = 'http://localhost:3001';
+// const productionURL = 'https://api-socketio-chat-web-production.up.railway.app';
+const developmentURL = 'http://localhost:3001';
 
 const socketURL = process.env.NODE_ENV === 'production' ? productionURL : developmentURL;
 
